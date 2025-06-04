c463cc5dff2c11000acd14d3cd680500f2d6a873
arnaldocloud <romeroarnaldo1@gmail.com>
2025-06-04 11:26:46 -0400

    actualizacion de dependencias

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Configuración adicional según sea necesario
+}
+
+module.exports = nextConfig
